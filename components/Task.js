41263b6d8407d2f50185834c66a4433ba79bfa28
jiahuiiiii <wongjiahui0915@gmail.com>
2022-06-13 14:29:05 +0800

    static data added

---
+++ components/Task.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Task() {
+  return (
+    <div>
+      <div className="bg-white shadow-md hover:shadow-2xl transition-all p-5 rounded-md">
+        <div className="text-md font-bold text-neutral-700">
+          Build UI for onboarding flow
+        </div>
+        <div className="text-zinc-500 text-sm">0 of 3 subtasks</div>
+      </div>
+    </div>
+  );
+}
+
+export default Task;
